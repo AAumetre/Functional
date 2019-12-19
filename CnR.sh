a051f056a928a85470aed304f608f596b4a17173
Antonin Aumètre <antonin.aumetre@gmail.com>
2019-12-19 23:10:34 +0100

    Added further composition

--- CnR.sh
+++ CnR.sh
@@ -1,3 +1,4 @@
+clear
 clang++ Main.cpp -o a -std=c++17 -I $HOME/COTS/doctest/doctest
 
 ./a
--- Main.cpp
+++ Main.cpp
@@ -10,43 +10,34 @@ TEST_CASE("Increment a value"){
     CHECK_EQ(2, 2);
 }
 
-/*
-int main(int argc, char** argv) {
-	std::cout << "Hello world" << std::endl;
+auto incr = [](const int value){
+    return value + 1;
+};
 
-	std::vector< int > my_numbers{ 1, 2, 3, 4, 5, 6, 7, 8, 9};
-	// Does the comparison in a lambda
-	auto sorted_numbers = pure::filter(my_numbers,
-			[](int n) {return (n % 2 == 0); });
-	for (const auto& num : sorted_numbers )
-		std::cout << num;
-	std::cout << std::endl;
+auto multDec = [](const int first){
+    return [=](const int second){return first*second;};
+};
 
-	// Uses the lambda to call an actual function ...
-	sorted_numbers = pure::filter(my_numbers,
-			[](int n) {return pure::ff_pair(n); });
-	for (const auto& num : sorted_numbers)
-		std::cout << num;
-	std::cout << std::endl;
+TEST_CASE("Multiply and increment"){
+    auto incMult = [](int first, int second){
+        return pure::cmp(incr, multDec(first))(second);
+    };
 
-	// Demo of tuples
-	const std::tuple< int, int > augmented = pure::incrementBoth(1, 2);
-	std::cout << std::get<0>( augmented ) << " "
-		<< std::get<1>( augmented ) << std::endl;
+    CHECK_EQ(incMult(3,4), 13);
+}
 
-	auto incrBef = []( int& value ) {
-		return ++value;
-	};
-	auto incrAft = [](int& value) {
-		return value++;
-	};
-
-	int val1 = 0, val2 = 0;
-	std::cout << "Initial: " << val1 << " => "
-		<< incrBef(val1) << " from " << val1 << std::endl;
-	std::cout << "Initial: " << val2 << " => "
-		<< incrAft(val2) << " from " << val2 << std::endl;
-	// 1 from 1
-	// 0 from 1
-	return 0;
+auto fourIncr = [](const int initial_value){
+    return pure::cmp( incr, incr )(
+            pure::cmp( incr, incr)(
+                initial_value
+                )
+            );
+};
+TEST_CASE("Composition"){
+    CHECK_EQ( fourIncr(6), 10 );
+}
+/*for (int i=0 ; i<10 ; ++i){
+    
 }*/
+
+
